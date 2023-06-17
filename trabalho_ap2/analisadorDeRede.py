90effad5a480eab6392ac60b8a40a88d032ea029
Gustavo Valentim <guuss.valentim@outlook.com>
2023-06-17 15:58:08 -0300

    v2

--- trabalho_ap2/analisadorDeRede.py
+++ trabalho_ap2/analisadorDeRede.py
@@ -1,8 +1,29 @@
-import scapy.all as scapy
+from scapy.all import *
+import datetime 
 
 
-class analisadorDeRede():
-    def __init__(self, arquivoInicializacao, periodoTarefas):
-        self.arquivo = open(arquivoInicializacao, "r")
-        self.periodicidade = periodoTarefas
+def __init__(self, arquivoInicializacao, periodoTarefas):
+    self.arquivo = open(arquivoInicializacao, "r")
+    self.periodicidade = periodoTarefas
         
+    
+def analisePacotes(pacote):
+    horario = datetime.datetime.now().strftime("%H:%M:%S")
+        
+    if TCP in pacote or UDP in pacote:
+        camada = "Transporte"
+        if TCP in pacote:
+            protocolo = "TCP"
+        elif UDP in pacote:
+            protocolo = "UDP"
+                
+    elif IP in pacote or IPv6 in pacote:
+        IP_host = pacote[IP].src
+        camada = "Rede"
+    elif Ether in pacote:
+        camada = "Enlace"
+    else:
+        camada = "Desconhecida"
+
+    yield f"{horario}, {camada}, {protocolo}, {IP_host}, {origem}, {destino}, {anomalia}, {descricao}"
+
